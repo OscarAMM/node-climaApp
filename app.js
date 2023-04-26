b214403cb1f541b16dd08349b949ba3684a4e152
Oscar Monsreal <A15216352@alumnos.uady.mx>
2023-04-25 19:32:03 -0600

    [Update] Included the method from search model

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const { leerInput, inquirerMenu, inquirerPausa } = require("./helpers/inquirer");
 const Busquedas = require("./models/busquedas");
 
@@ -13,18 +14,19 @@ const main = async () => {
                 case 1:
                     //Show message
                     const place = await leerInput('Ciudad: ');
-                    
+                    const lugar = busquedas.ciudad(place);
+                    console.log(lugar);
                     //Search places
                     //Select the place
                     //Weather [Geolocation]
                     //Show results
-                    console.log('\nInformación de la ciudad\n');
-                    console.log('Ciudad: ');
-                    console.log('Lng: ');
-                    console.log('Ltd: ');
-                    console.log('Temperature: ');
-                    console.log('Max: ');
-                    console.log('Min: ');
+                    // console.log('\nInformación de la ciudad\n');
+                    // console.log('Ciudad: ');
+                    // console.log('Lng: ');
+                    // console.log('Ltd: ');
+                    // console.log('Temperature: ');
+                    // console.log('Max: ');
+                    // console.log('Min: ');
                     break;
                 case 2:
                     console.log('2 dentro de la opción');
